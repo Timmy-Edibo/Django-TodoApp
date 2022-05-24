be8a5d04f96e39c7341def33914553cd9e44ed4a
timmy-edibo <timothyedibo@gmail.com>
2022-05-24 13:29:44 +0100

    created_at field in models adjusted from auto_create to auto_now

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
